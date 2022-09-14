80306a0627fc12d67165cc1ed636d3220ca6e552
vaibhav pendhare <vaibhav.pendhare@clouddrove.com>
2022-09-14 13:34:13 +0530

    updated main.tf

--- _example/cloudwatch-billing-alert-to-pre-existing-sns-consolidated-acct/main.tf
+++ _example/cloudwatch-billing-alert-to-pre-existing-sns-consolidated-acct/main.tf
@@ -6,5 +6,5 @@ module "billing_cloudwatch_alert" {
   label_order               = ["name", "environment"]
   monthly_billing_threshold = 500
   currency                  = "USD"
-  aws_sns_topic_arn         = ["arn:aws:lambda:us-east-1:111111111111:function:bb-root-org-notify_slack"]
+  # aws_sns_topic_arn         = ["arn:aws:lambda:us-east-1:111111111111:function:bb-root-org-notify_slack"]
 }
--- _example/cloudwatch-billing-alert-to-pre-existing-sns-with-acct-id/main.tf
+++ _example/cloudwatch-billing-alert-to-pre-existing-sns-with-acct-id/main.tf
@@ -7,5 +7,5 @@ module "billing_cloudwatch_alert" {
   aws_account_id            = 111111111111
   monthly_billing_threshold = 500
   currency                  = "USD"
-  aws_sns_topic_arn         = ["arn:aws:lambda:us-east-1:111111111111:function:bb-root-org-notify_slack"]
+  # aws_sns_topic_arn         = ["arn:aws:lambda:us-east-1:111111111111:function:bb-root-org-notify_slack"]
 }
